89469eec82728f849c6dcdb0249af890538f8195
yokesh_ks <yokesh.ks@increscotech.com>
2023-02-18 23:45:11 +0530

    feat(shared package): created and web integrated

---
+++ packages/shared/src/App.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>NativeAnimo</Text>
+    </View>
+  )
+}
+
+export default App
--- packages/web/src/index.js
+++ packages/web/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Text } from 'react-native'
+import App from 'shared/src/App'
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
-    <Text>Hello</Text>
+    <App />
   </React.StrictMode>,
 )
